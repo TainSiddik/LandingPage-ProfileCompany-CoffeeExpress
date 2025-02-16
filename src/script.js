44d099630eb51197bb03f910fa92b23c6549531c
TainSiddik <tainsiddik11@gmail.com>
2025-02-16 17:31:17 +0700

    finall building v.1.0

--- src/script.js
+++ src/script.js
@@ -1,18 +1,57 @@
-const bg = document.getElementById('bg')
-const bn = document.getElementById('bn')
-const gambar = document.getElementById('gambar')
-const text = document.getElementById('text')
+// navDesktop
+const navLinks = document.querySelectorAll('.nav a');
 
+const changeLinkColorOnClick = () => {
+    navLinks.forEach(link => {
+        link.onclick = () => {
+            // Menghapus kelas 'text-orange-200' dari semua link
+            navLinks.forEach(item => item.classList.remove('text-orange-200'));
 
-const cola = () => {
-    bg.style.backgroundColor = '#dc2626'
-    bn.style.backgroundColor = '#0f172a'
-    text.style.color = '#f1f5f9'
-    gambar.src = "./img/coca-cola.png";
+            // Menambahkan kelas 'text-orange-200' pada link yang diklik
+            link.classList.add('text-orange-200');
+        };
+    });
+};
+
+// Menambahkan logika untuk "Home" yang berwarna dari awal
+const setInitialActiveLink = () => {
+    const homeLink = document.querySelector('.hero'); // Mengambil link Home
+    homeLink.classList.add('text-orange-200'); // Memberikan kelas 'text-orange-200' pada link Home
+};
+
+setInitialActiveLink();
+changeLinkColorOnClick();
+
+// end of navDesktop
+
+// mobileNav
+const mobileNav = document.getElementById('mobileNav')
+const icon = document.getElementById('icon')
+
+const btnNav = () => {
+    mobileNav.classList.toggle("translate-x-full")
+    if (icon.classList.contains("fa-bars")) {
+        icon.classList.replace("fa-bars", "fa-x")
+    } else {
+        icon.classList.replace("fa-x", "fa-bars")
+    }
 }
-const sprite = () => {
-    bg.style.backgroundColor = '#4ade80'
-    bn.style.backgroundColor = '#bbf7d0'
-    text.style.color = '#0f172a'
-    gambar.src = "./img/sprite.png"
+
+const navMobile = document.querySelectorAll('.mobile a')
+const changeLinkColorOnMobile = () => {
+    navMobile.forEach(link => {
+        link.onclick = () => {
+            navMobile.forEach(item => item.classList.remove('text-orange-200'))
+            link.classList.add('text-orange-200')
+        };
+    });
+};
+
+const setInitialActiveLinkMobile = () => {
+    const homeLink = document.querySelector('.mobileHero')
+    homeLink.classList.add('text-orange-200')
 }
+
+setInitialActiveLinkMobile()
+changeLinkColorOnMobile()
+// end of mobile Nav
